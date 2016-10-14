e6c74d207e4e196cc26bd75e6a31f17c8b17bea6
Steve Peters <scpeters@osrfoundation.org>
2016-10-14 13:47:05 -0700

    don't send stop command to harness, just let it lower all the way

--- scripts/init_robot.sh
+++ scripts/init_robot.sh
@@ -6,12 +6,8 @@ sleep 60
 echo "lower harness"
 rostopic pub -1 /valkyrie/harness/velocity std_msgs/Float32 '{data: -0.05}'
 
-echo "stop lowering after 20 seconds (optional)"
+echo "switch to high level control after 20 seconds"
 sleep 20
-rostopic pub -1 /valkyrie/harness/velocity std_msgs/Float32 '{data: 0}'
-
-echo "switch to high level control in 3 seconds"
-sleep 3
 rostopic pub -1 /ihmc_ros/valkyrie/control/low_level_control_mode ihmc_valkyrie_ros/ValkyrieLowLevelControlModeRosMessage '{requested_control_mode: 2, unique_id: -1}'
 
 echo "detach in 5 seconds"
