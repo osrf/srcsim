c19c2ce58ffca24c873f981ce468320d8777f24b
Steve Peters <scpeters@osrfoundation.org>
2016-10-14 13:46:26 -0700

    detach without pausing

--- scripts/init_robot.sh
+++ scripts/init_robot.sh
@@ -14,13 +14,9 @@ echo "switch to high level control in 3 seconds"
 sleep 3
 rostopic pub -1 /ihmc_ros/valkyrie/control/low_level_control_mode ihmc_valkyrie_ros/ValkyrieLowLevelControlModeRosMessage '{requested_control_mode: 2, unique_id: -1}'
 
-echo "pause then detach in 5 seconds"
+echo "detach in 5 seconds"
 sleep 5
-gz world -p 1
-sleep 1
 rostopic pub -1 /valkyrie/harness/detach std_msgs/Bool true &
-sleep 1
-gz world -p 0
 
 if [ $1 = "true" ]; then
   echo "start walking"
