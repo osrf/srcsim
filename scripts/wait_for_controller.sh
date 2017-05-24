39f77294e7ac5247264b4f12d6fe6094f86ac061
Ian Chen <ichen@osrfoundation.org>
2017-05-24 18:34:50 +0000

    check rosparam instead

--- scripts/wait_for_controller.sh
+++ scripts/wait_for_controller.sh
@@ -2,11 +2,13 @@
 
 bridge=""
 while [ -z "$bridge" ]; do
-  bridge=`rosservice call /controller_manager/list_controllers | grep ihmc_valkyrie_control_java_bridge`
+  bridge=`rosparam list | grep ihmc_valkyrie_control_java_bridge`
   sleep 1
   echo "waiting for ihmc_valkyrie_control_java_bridge"
 done
 
 echo "Found ihmc_valkyrie_control_java_bridge! Launching grasp_init."
+
+sleep 5
 
 roslaunch srcsim grasping_init.launch
